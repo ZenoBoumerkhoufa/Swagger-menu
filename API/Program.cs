d3467d2faa4ecad4aaed96999fc64bd1c809236c
ZenoBoumerkhoufa <boumerkhoufazeno@gmail.com>
2023-05-31 10:49:58 +0200

    aanpassingen evaluatie

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,6 @@
+using API;
+using System.Threading.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +10,58 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+builder.Services.AddHealthChecks();
+#region rate limiting
+builder.Services.AddRateLimiter(options =>
+{
+    options.OnRejected = async (context, token) =>
+    {
+        context.HttpContext.Response.StatusCode = 429; // 418: Teapot
+        if (context.Lease.TryGetMetadata(MetadataName.RetryAfter, out var retryAfter))
+        {
+            await context.HttpContext.Response.WriteAsync(
+            $"Too many requests. Please try again after {retryAfter.TotalMinutes} minute(s)." /*+ $"Read more about our rate limits at https://example.org/docs/ratelimiting."*/, cancellationToken: token);
+        }
+        else
+        {
+            await context.HttpContext.Response.WriteAsync(
+            "Too many requests. Please try again later." /* + "Read more about our rate limits at https://example.org/docs/ratelimiting."*/, cancellationToken: token);
+        }
+    }; options.GlobalLimiter = PartitionedRateLimiter.CreateChained(
+    PartitionedRateLimiter.Create<HttpContext, string>(httpContext =>
+    RateLimitPartition.GetFixedWindowLimiter(httpContext.ResolveClientIpAddress(), partition =>
+    new FixedWindowRateLimiterOptions
+    {
+        AutoReplenishment = true,
+        PermitLimit = 600,
+        Window = TimeSpan.FromMinutes(1)
+    })),
+    PartitionedRateLimiter.Create<HttpContext, string>(httpContext =>
+    RateLimitPartition.GetFixedWindowLimiter(httpContext.ResolveClientIpAddress(), partition =>
+    new FixedWindowRateLimiterOptions
+    {
+        AutoReplenishment = true,
+        PermitLimit = 6000,
+        Window = TimeSpan.FromHours(1)
+    })),
+    PartitionedRateLimiter.Create<HttpContext, string>(httpContext =>
+   RateLimitPartition.GetFixedWindowLimiter(httpContext.ResolveClientIpAddress(), partition =>
+    new FixedWindowRateLimiterOptions
+    {
+        AutoReplenishment = true,
+        PermitLimit = 10,
+        QueueLimit = 6,
+        QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+        Window = TimeSpan.FromSeconds(1)
+    }))
+    );
+});
+#endregion
+
+var app = builder.Build(); 
+app.MapHealthChecks("/Health");
+app.UseRateLimiter();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
